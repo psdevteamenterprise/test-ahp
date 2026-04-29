01b36ec6c72cae73a45ee74661bb85e43f034cb1
Matheus <107147983+matheuswix@users.noreply.github.com>
2026-04-29 14:26:11 -0300

    Revert "test logs 10.0.74"
    This reverts commit 08d2efe2b266596efc39ec59b70184556afc82e6.

--- src/pages/New Fast Homepage.tdltw.js
+++ src/pages/New Fast Homepage.tdltw.js
@@ -1,4 +1,3 @@
-import { logHomePageLoadPhase } from 'abmp-npm/public/Utils/homePageLoadTrace';
 import {
   getCompiledFiltersOptions,
   getNonCompiledFiltersOptions,
@@ -6,10 +5,7 @@ import {
 } from 'backend/web-methods.web.js';
 import { homePageOnReady } from 'abmp-npm/pages';
 
-logHomePageLoadPhase('page_script_executed_after_imports');
-
 $w.onReady(async function () {
-  logHomePageLoadPhase('wix_onready_callback_start');
   await homePageOnReady({
     _$w: $w,
     getCompiledFiltersOptions,
